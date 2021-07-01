d80ca435298cad034f1dc37ecea22ff564afa3bd
Sergey Zhirkov <zhirkov_55250@mail.ru>
2021-07-01 11:14:13 +0500

    homework 3

---
+++ smarket/src/main/java/com/sergeyzhirkov/market/controllers/MainController.java
@@ -0,0 +1,40 @@
+package com.sergeyzhirkov.market.controllers;
+
+import com.sergeyzhirkov.market.model.Product;
+import com.sergeyzhirkov.market.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class MainController {
+    private ProductService productService;
+
+    @Autowired
+    public MainController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/all")
+    public String showAllProducts(Model model) {
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        return "all_products";
+    }
+
+    @GetMapping("/create_product_form")
+    public String showCreateProductForm() {
+        return "add_form";
+    }
+
+    @PostMapping("/create_new_product")
+    public String createNewProduct(@RequestParam Integer id, @RequestParam String name, @RequestParam Integer price) {
+        productService.addProduct(new Product(id, name, price));
+        return "redirect:/all";
+    }
+}
---
+++ smarket/src/main/java/com/sergeyzhirkov/market/model/Product.java
@@ -0,0 +1,34 @@
+package com.sergeyzhirkov.market.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private int price;
+
+    public Product(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
